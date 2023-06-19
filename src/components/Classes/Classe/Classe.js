eb1c3a5151451012c7cb7b36718d89205bc1752a
Ubergue De Andrade <bergjr.andrade@gmail.com>
2023-06-19 12:57:30 +0100

    Some minor changes

--- src/components/Classes/Classe/Classe.js
+++ src/components/Classes/Classe/Classe.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import styles from './Classe.module.scss';
+import bag from 'assets/duffle-bag.png'
+
 const Classe = (props) => {
     const img = require(`assets/${props.img}`)
     
     return (
-        <li className={styles['class']} style={{backgroundImage: `url(${img})`}}>
-                <div className={styles.icon}></div>
-                <div className={styles.text}>
-                    <h3>{props.exercise}</h3>
-                    <h4>{props.time}</h4>
-                </div>    
+
+        <li className={styles['classe']}>
+            <div className={styles.img} style={{backgroundImage: `url(${img})`}}></div>
+            <div className={styles.icon}>
+                <img src={bag} alt="Red bag"/>
+            </div>
+            <div className={styles['text']}>
+                <h3>{props.exercise}</h3>
+                <h4>{props.time}</h4>
+            </div>
         </li>
-    );
+    )
 }
 
 export default Classe;
